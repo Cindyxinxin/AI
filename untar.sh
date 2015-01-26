9dc9bea60ee2c05f8ffa11d2a86c078dbe8ae90d
Xin (Cindy) Yi <yx0618@cs.washington.edu>
2015-01-25 23:22:00 -0800

    add untar file

---
+++ untar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$#" -eq  "1" ] 
+	then
+	mkdir output
+	cd $1
+	for file in *.tar.gz
+	do
+  		tar -vxzf "$file" -C "../output"
+	done
+	cd ../output
+	ls | grep -v .csv$| xargs rm
+else
+echo "usage: ./untar folder"
+fi
